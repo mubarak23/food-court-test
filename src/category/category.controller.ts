8d1a2c214851aa72f5de466c8bed554c9715339f
mubarak23 <mubarakaminu340@gmail.com>
2023-01-08 15:52:52 +0100

    only an admin can create and view categories

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -1,4 +1,7 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { RoleGuard } from 'src/auth/role.guard';
+import { Roles } from 'src/auth/roles.decorator';
 import { CategoryService } from './category.service';
 import { CreateCategoryDto } from './dto/create-category.dot';
 
@@ -6,11 +9,15 @@ import { CreateCategoryDto } from './dto/create-category.dot';
 export class CategoryController {
   constructor(private categoryService: CategoryService) {}
 
+  @Roles('ADMIN')
+  @UseGuards(JwtAuthGuard, RoleGuard)
   @Post('')
   async createCategory(@Body() props: Partial<CreateCategoryDto>) {
     return this.categoryService.createCategory(props);
   }
 
+  @Roles('ADMIN')
+  @UseGuards(JwtAuthGuard, RoleGuard)
   @Get('/all')
   async getAllCategories() {
     const addons = await this.categoryService.findallCategories();
